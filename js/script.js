5086ee9c2b7acb9bfc836ddd6defb1069bfea66b
Ramiro Lupo <luporamiro5@gmail.com>
2022-02-14 16:44:57 -0300

    primer desafío entregable

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//Definición de variables
+
+let playaIzq = 5;
+let playaDer = 2;
+let playaCentral = 0;
+
+
+let ingreso = parseInt(prompt("                                 ESTACIONAMIENTO RUPERTI\n\n Menu principal:\n\n1 - Estacionar playa izquierda.\n2 - Estacionar playa central.\n3 - Estacionar playa derecha.\n4 - Salir"));
+
+while (ingreso != 4 && ingreso != 2) {
+
+    switch (ingreso) {
+        case 1: //Estacionar playa izquierda
+            alert("Disculpa campeón pero está llena, elegí otra");
+            break;
+        
+        case 2: //Estacionar playa central
+            alert("Hay lugar, acordate que lo dejaste en la playa central");
+            break;
+
+        case 3: //Estacionar playa derecha
+            alert("Hay lugar, acordate que lo dejaste en la playa derecha");
+            break;
+
+        case 4: //Salir
+            alert("Muchas gracias por venir a estacionamiento Ruperti!");
+            break;
+
+        default:
+            alert("Te equivocaste de opción rey, probá de nuevo");
+            break;
+    } 
+
+    ingreso = parseInt(prompt("                                 ESTACIONAMIENTO RUPERTI\n\n Menu principal:\n\n1 - Estacionar playa izquierda.\n2 - Estacionar playa central.\n3 - Estacionar playa derecha.\n4 - Salir"));
+
+}
