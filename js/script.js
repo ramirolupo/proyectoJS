e8f89a9b371138097b27b936bdadc5037c578fe2
Ramiro Lupo <luporamiro5@gmail.com>
2022-03-09 19:57:17 -0300

    entrega eventos

--- js/script.js
+++ js/script.js
@@ -1,21 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     hacerCards()
-})
+});
 
 const planes = [];
-let carrito
-
-let validarCarrito = localStorage.getItem("carrito")
-
-if(validarCarrito == null){
-    carrito = []
-    console.log(carrito)
-
-}else{
-    carrito = JSON.parse(validarCarrito)
-    console.log(carrito)
-}
+const carrito = [];
 
 
 class PlanesDieta {
@@ -24,23 +13,20 @@ class PlanesDieta {
         this.id = id;
         this.name = nombre;
         this.price = precio;
-        this.img = imgen
+        this.img = imgen;
 
     }
 };
 
 //globales 
 
-let cardsHtml = document.getElementById("card")
+let cardsHtml = document.getElementById("card");
 
 //resto del codigo
 planes.push(new PlanesDieta(15353,"Plan de Entrenamiento", 2000, "https://api.nutricionistasofiadiloreto.com.ar/api/tratamientos/imagen/0f272af6-1c8d-4104-471b-08d655720ca3"));
 planes.push(new PlanesDieta(23535,"Plan Vegetariano", 1000, "https://api.nutricionistasofiadiloreto.com.ar/api/tratamientos/imagen/27e67577-6b6d-49d9-471f-08d655720ca3"));
 planes.push(new PlanesDieta(3333,"Plan Completo", 4000, "https://api.nutricionistasofiadiloreto.com.ar/api/tratamientos/imagen/dbd574b7-4a2a-4228-65bd-08d7dbf48c15"));
 
-
-
-//crear => () {} sacar turno y pedir los diferentes datos para crear el objeto y pushearlo al array
 
 const hacerCards = () => {
 
@@ -67,33 +53,6 @@ const hacerCards = () => {
         console.log(buttonHtml)
         buttonHtml.addEventListener('click', () => agregarAlCarrito(plan))
 
-        // button.onclick = () => {console.log(`Forma n2 buton: ${plan.id}`)}
-
     }
 
 };
-
-const agregarAlCarrito = (item) => {
-
-    console.log(item)
-
-    let aux = carrito.find(Element => Element.id === item.id)
-
-    if(aux === undefined){
-        carrito.push(item)
-        alert("Añadido")
-        guardarStorage()
-    }else{
-        alert("Ya esta añadido el plan")
-    }
-
-}
-
-const vaciarCarrito = () => {
-    carrito = []
-    guardarStorage()
-}
-
-const guardarStorage = () => {
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-}
