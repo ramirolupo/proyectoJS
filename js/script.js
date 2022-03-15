880fb052edb35c2c9922ebf8724aa419709031d8
Ramiro Lupo <luporamiro5@gmail.com>
2022-03-15 19:07:08 -0300

    segunda entrega proyecto

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    hacerCards()
+document.addEventListener("DOMContentLoaded", () => {
+  hacerCards();
 });
 
 const planes = [];
@@ -8,47 +7,62 @@ let carrito;
 
 let validarCarrito = localStorage.getItem("carrito");
 
-if(validarCarrito == null){
-    carrito = [];
-    console.log(carrito);
-
-}else{
-    carrito = JSON.parse(validarCarrito);
-    console.log(carrito);
+if (validarCarrito == null) {
+  carrito = [];
+  console.log(carrito);
+} else {
+  carrito = JSON.parse(validarCarrito);
+  console.log(carrito);
 }
 
 class PlanesDieta {
-    constructor(id, nombre, precio, imgen){
+  constructor(id, nombre, precio, imgen) {
+    this.id = id;
+    this.name = nombre;
+    this.price = precio;
+    this.img = imgen;
+  }
+}
 
-        this.id = id;
-        this.name = nombre;
-        this.price = precio;
-        this.img = imgen;
-
-    }
-};
-
-//globales 
+//globales
 
 let cardsHtml = document.getElementById("card");
 
 //resto del codigo
-planes.push(new PlanesDieta(1,"Plan de Entrenamiento", 2000, "https://api.nutricionistasofiadiloreto.com.ar/api/tratamientos/imagen/0f272af6-1c8d-4104-471b-08d655720ca3"));
-planes.push(new PlanesDieta(2,"Plan Vegetariano", 1000, "https://api.nutricionistasofiadiloreto.com.ar/api/tratamientos/imagen/27e67577-6b6d-49d9-471f-08d655720ca3"));
-planes.push(new PlanesDieta(3,"Plan Completo", 4000, "https://api.nutricionistasofiadiloreto.com.ar/api/tratamientos/imagen/dbd574b7-4a2a-4228-65bd-08d7dbf48c15"));
-
+planes.push(
+  new PlanesDieta(
+    1,
+    "Plan de Entrenamiento",
+    2000,
+    "https://api.nutricionistasofiadiloreto.com.ar/api/tratamientos/imagen/0f272af6-1c8d-4104-471b-08d655720ca3"
+  )
+);
+planes.push(
+  new PlanesDieta(
+    2,
+    "Plan Vegetariano",
+    1000,
+    "https://api.nutricionistasofiadiloreto.com.ar/api/tratamientos/imagen/27e67577-6b6d-49d9-471f-08d655720ca3"
+  )
+);
+planes.push(
+  new PlanesDieta(
+    3,
+    "Plan Completo",
+    4000,
+    "https://api.nutricionistasofiadiloreto.com.ar/api/tratamientos/imagen/dbd574b7-4a2a-4228-65bd-08d7dbf48c15"
+  )
+);
 
 const hacerCards = () => {
+  cardsHtml.innerHTML = ``;
 
-    cardsHtml.innerHTML = ``
+  for (plan of planes) {
+    console.warn(plan.name);
 
-    for(plan of planes){
-
-        console.warn(plan.name);
-
-        let card = document.createElement('div');
-        card.className = "card-list"
-        card.innerHTML = `
+    let card = document.createElement("div");
+    card.className = "card-list";
+    card.innerHTML = `
             <div class="card-image">
                 <img src=${plan.img} alt="Anastase Maragos"/>
             </div>
@@ -57,45 +71,36 @@ const hacerCards = () => {
                 <p>$${plan.price}/Mes</p>
                 <button id="${plan.id}" onclick="triggerBtnEvent(${plan.id})" class="w3-btn w3-red w3-margin-top w3-round" style="width:100%" >Elegir Plan</button>
             </div>
-        `
-        cardsHtml.appendChild(card)
-
-    };
-
+        `;
+    cardsHtml.appendChild(card);
+  }
 };
 
-function triggerBtnEvent(id){
-
-    console.log(`Prueba boton: ${id}`);
-    
-};
-
-triggerBtnEvent.addEventListener('click', () => agregarAlCarrito(plan));
+function triggerBtnEvent(id) {
+  console.log(`Prueba boton: ${id}`);
+  agregarAlCarrito(plan);
+}
 
 const agregarAlCarrito = (item) => {
+  console.log(item);
 
-    console.log(item);
+  let aux = carrito.find((Element) => Element.id === item.id);
 
-    let aux = carrito.find(Element => Element.id === item.id);
-
-    if(aux === undefined){
-        carrito.push(item);
-        alert("Añadido");
-        guardarStorage();
-    }else{
-        alert("Ya esta añadido el plan");
-    }
-
-}
+  if (aux === undefined) {
+    carrito.push(item);
+    alert("Añadido");
+    guardarStorage();
+  } else {
+    alert("Ya esta añadido el plan");
+  }
+};
 
 const vaciarCarrito = () => {
+  carrito = [];
 
-    carrito = [];
-
-    guardarStorage();
-}
+  guardarStorage();
+};
 
 const guardarStorage = () => {
-
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-}
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
